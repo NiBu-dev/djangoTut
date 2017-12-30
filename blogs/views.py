1d5edfc15fc865429726378b2b14032542dd8604
sahas023 <sahas023@gmail.com>
2017-12-30 21:47:50 +0100

    blog done

---
+++ blogs/views.py
@@ -0,0 +1,26 @@
+# blog_app/views.py
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from . models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'text']
+    template_name = 'post_edit.html'
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
+    'posts',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
